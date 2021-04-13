c083cbc80117ea9e51f188842f45a71334592130
MaurizioBonanno <mauriziobonanno43@gmail.com>
2021-04-13 22:48:56 +0200

    installato three shake

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import component from './component';
 import "./main.css";
 import "react";
 import "react-dom";
-
+import { bake } from './shake';
 
 document.body.appendChild(component());
+
+bake();
