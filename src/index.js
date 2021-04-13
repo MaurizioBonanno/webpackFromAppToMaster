7880a51d43eaa09d6b281195fa6e5048b4ddd99d
MaurizioBonanno <mauriziobonanno43@gmail.com>
2021-04-14 00:25:59 +0200

    aggiunto threejs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./main.css";
 import "react";
 import "react-dom";
 import { bake } from './shake';
-
+import * as THREE from '../node_modules/three/build/three.module';
 document.body.appendChild(component());
 
 bake();
